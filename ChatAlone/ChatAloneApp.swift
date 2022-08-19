2b69cd6bbc1c6f8fd519e4a8efedeebc89f6934a
Min-Su Kim <min-sukim@Min-Suui-MacBookPro-2.local>
2022-08-19 18:12:39 +0900

    프로젝트 생성

---
+++ ChatAlone/ChatAloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAloneApp.swift
+//  ChatAlone
+//
+//  Created by Min-Su Kim on 2022/08/19.
+//
+
+import SwiftUI
+
+@main
+struct ChatAloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAlone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatAlone
+//
+//  Created by Min-Su Kim on 2022/08/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
